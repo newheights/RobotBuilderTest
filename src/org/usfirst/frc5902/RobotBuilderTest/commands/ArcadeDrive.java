c56db7144b0aaaa1badd85538412cd36660c090b
FRCTeam5902 <frc5902@newheightsonline.org>
2016-03-03 15:51:59 -0500

    Update ArcadeDrive.java

--- src/org/usfirst/frc5902/RobotBuilderTest/commands/ArcadeDrive.java
+++ src/org/usfirst/frc5902/RobotBuilderTest/commands/ArcadeDrive.java
@@ -8,7 +8,7 @@
 // update. Deleting the comments indicating the section will prevent
 // it from being updated in the future.
 
-
+// Full speed driver control
 package org.usfirst.frc5902.RobotBuilderTest.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
