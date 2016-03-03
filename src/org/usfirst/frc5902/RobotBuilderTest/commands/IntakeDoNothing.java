ac917aa80826aa1a3ac8920cbf726bc33259ade6
FRCTeam5902 <frc5902@newheightsonline.org>
2016-03-03 16:39:26 -0500

    Update IntakeDoNothing.java

--- src/org/usfirst/frc5902/RobotBuilderTest/commands/IntakeDoNothing.java
+++ src/org/usfirst/frc5902/RobotBuilderTest/commands/IntakeDoNothing.java
@@ -15,7 +15,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc5902.RobotBuilderTest.Robot;
 
 /**
- *
+Stops the flap from spinning
  */
 public class IntakeDoNothing extends Command {
 
